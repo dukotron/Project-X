b32237139db73ca4a66f55ee7fa1159cc0daa140
Roko Milić <cybermen21@hotmail.com>
2017-04-15 16:53:10 +0200

    Camera movement, basic cube placement

---
+++ Assets/Scripts/CameraControls.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+
+    public float dragSpeed = 2f;
+    public float moveSpeed = 2f;
+
+    private Vector3 dragOrigin;
+    private float xDir;
+    private float yDir;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.Q))
+            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.E))
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftShift))
+            moveSpeed = 4f;
+        if (!Input.GetKey(KeyCode.LeftShift))
+            moveSpeed = 2f;
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Cursor.visible = false;
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+        if (!Input.GetMouseButton(1))
+        {
+            Cursor.visible = true;
+            return;
+        }
+
+        Vector3 mouseDir = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+
+        xDir += Mathf.Clamp(mouseDir.x, -0.3f, 0.3f) * dragSpeed;
+        yDir += Mathf.Clamp(-mouseDir.y, -0.2f, 0.2f) * dragSpeed;
+
+        //unity uses x rotation for up/down so placed yDir from mouse in x............
+        transform.rotation = Quaternion.Euler(yDir, xDir, 0f);
+    }
+
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform genericBlock;
+
+    private RaycastHit hit;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 incomingVec = hit.normal;
+
+                if (incomingVec == new Vector3(0, 1, 0))
+                    Instantiate(genericBlock, new Vector3(hit.transform.position.x, hit.transform.position.y + hit.transform.localScale.y, hit.transform.position.z), Quaternion.identity);
+                else if (incomingVec == new Vector3(0, -1, 0))
+                    Instantiate(genericBlock, new Vector3(hit.transform.position.x, hit.transform.position.y - hit.transform.localScale.y, hit.transform.position.z), Quaternion.identity);
+                else if (incomingVec == new Vector3(0, 0, 1))
+                    Instantiate(genericBlock, new Vector3(hit.transform.position.x, hit.transform.position.y, hit.transform.position.z + hit.transform.localScale.z), Quaternion.identity);
+                else if (incomingVec == new Vector3(0, 0, -1))
+                    Instantiate(genericBlock, new Vector3(hit.transform.position.x, hit.transform.position.y, hit.transform.position.z - hit.transform.localScale.z), Quaternion.identity);
+                else if (incomingVec == new Vector3(1, 0, 0))
+                    Instantiate(genericBlock, new Vector3(hit.transform.position.x + hit.transform.localScale.x, hit.transform.position.y, hit.transform.position.z), Quaternion.identity);
+                else if (incomingVec == new Vector3(-1, 0, 0))
+                    Instantiate(genericBlock, new Vector3(hit.transform.position.x - hit.transform.localScale.x, hit.transform.position.y, hit.transform.position.z), Quaternion.identity);
+
+                print("Block Added");
+            }
+        }
+    }
+}
